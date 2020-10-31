d7edff5681d54a06890177ac38402eaf785ff8eb
unknown <szymon.skalmierski123@gmail.com>
2020-10-31 01:20:58 +0100

    index

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "remotemysql.com";
+$username = "spaLcr81av";
+$password = "GkI5KlQALM";
+$dbname = "spaLcr81av";
+
+$conn=new mysqli($servername, $username, $password, $dbname);
+        if($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+?>
