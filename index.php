5d517546ed1f3a7b0af0b9eb315c788badb411b8
unknown <szymon.skalmierski123@gmail.com>
2020-11-01 01:47:06 +0100

    1

--- index.php
+++ index.php
@@ -5,34 +5,58 @@
 
 <?php
     
-    echo("<div style='font-size:30px; margin-bottom:20px'><b>Szymon Skalmierski nr26</b></div>");
-    echo("<hr />");
-    function tabelka($zapytanie, $nazwa, $kolumna, $row1){
-    include("connect.php");
-    $result=$conn->query($zapytanie);
-    echo("<div style='font-size:20px; margin-bottom:5px'>$nazwa</div>");
-    echo("<table border=1 style='font-size:20px; margin-bottom:20px'>");
-        echo("<th>$kolumna</th>");
-        echo("<th>Dział</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                    echo("<td>".$row[$row1]."</td><td>".$row['dzial']."</td>");
-                echo("</tr>");
-            }
-    echo("</table>");
-    }
-
-    tabelka("select avg(zarobki) as myavg, dzial from pracownicy group by dzial", "Średnie zarobki pracowników w poszczególnych działach:", "Średnia", "myavg");
-    echo("<hr />");
-    tabelka("select count(zarobki) as mycount, dzial from pracownicy where imie like '%a' group by dzial", "Liczba mężczyzn w poszczególnych działach:", "Liczba", "mycount");
-    echo("<hr />");
-    tabelka("select max(zarobki) as mymax, dzial from pracownicy where dzial=2 group by dzial", "Maksymalne zarobki z działu 2:", "Maks", "mymax");
-    echo("<hr />");
-    tabelka("select min(zarobki) as mymin, dzial from pracownicy where imie not like '%a' group by dzial", "Minimalne zarobki mężczyzn:", "Min", "mymin");
-    echo("<hr />");
-    tabelka("select sum(zarobki) as mysum, dzial from pracownicy where dzial=1 or dzial=2 group by dzial", "Suma zarobków z działu 1 i 2:", "Suma", "mysum");
-    echo("<hr />");
-    tabelka("select avg(zarobki) as myavg, dzial from pracownicy group by dzial having myavg<35", "Średnie zarobki pracowników w poszczególnych działach mniejsze od 35:", "Średnia", "myavg");
+    echo("<div style='font-size:40px; margin-bottom:20px; font-family:helvetica; text-align:center'><b>Szymon Skalmierski nr26</b></div>");
     echo("<hr />");
 
+
+    function tabelka1($zapytanie, $nazwa, $kolumna, $row1){
+        require("connect.php");
+        $result=$conn->query($zapytanie);
+        echo("<div style='font-size:20px; margin-bottom:5px'>$nazwa</div>");
+        echo("<table border=1 style='font-size:20px; margin-bottom:20px'>");
+            echo("<th>$kolumna</th>");
+            echo("<th>Dział</th>");
+                while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                        echo("<td>".$row[$row1]."</td><td>".$row['dzial']."</td>");
+                    echo("</tr>");
+                }
+        echo("</table>");
+        echo("<hr>");
+        }
+
+
+    function tabelka4($zapytanie, $nazwa){
+        require("connect.php");
+        $result=$conn->query($zapytanie);
+        echo("<div style='font-size:20px; margin-bottom:5px'>$nazwa</div>");
+        echo("<table border=1 style='font-size:20px; margin-bottom:20px'>");
+            echo("<th>ID</th>");
+            echo("<th>Imie</th>");
+            echo("<th>Dział</th>");
+            echo("<th>Zarobki</th>");
+                while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                    echo("</tr>");
+                }
+        echo("</table>");
+        echo("<hr>");
+        }
+
+    tabelka4("select * from pracownicy", "Wszyscy pracownicy");
+    
+    tabelka1("select avg(zarobki) as myavg, dzial from pracownicy group by dzial", "Średnie zarobki pracowników w poszczególnych działach:", "Średnia", "myavg");
+
+    tabelka1("select count(zarobki) as mycount, dzial from pracownicy where imie like '%a' group by dzial", "Liczba mężczyzn w poszczególnych działach:", "Liczba", "mycount");
+
+    tabelka1("select max(zarobki) as mymax, dzial from pracownicy where dzial=2 group by dzial", "Maksymalne zarobki z działu 2:", "Maks", "mymax");
+
+    tabelka1("select min(zarobki) as mymin, dzial from pracownicy where imie not like '%a' group by dzial", "Minimalne zarobki mężczyzn:", "Min", "mymin");
+
+    tabelka1("select sum(zarobki) as mysum, dzial from pracownicy where dzial=1 or dzial=2 group by dzial", "Suma zarobków z działu 1 i 2:", "Suma", "mysum");
+ 
+    tabelka1("select avg(zarobki) as myavg, dzial from pracownicy group by dzial having myavg<35", "Średnie zarobki pracowników w poszczególnych działach mniejsze od 35:", "Średnia", "myavg");
+
+    
 ?>
