104a6a6e6978ef5bd417a743e64fdda39b5d3cf7
Szymon Skalmierski <73532434+szymon-skalmierski@users.noreply.github.com>
2020-11-04 14:38:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 
 <h1>Szymon Skalmierski nr26</h1>
-<h2><a href="https://github.com/SK-2019/php-sql-wprowadzenie-szymon-skalmierski">Github</a><h2>
+<div class="link"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-szymon-skalmierski">Github</a><h2>
 
 </body>
 
