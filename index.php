104ba25451060fa7f1fbd0bd9255fea5c7013dc5
Szymon Skalmierski <73532434+szymon-skalmierski@users.noreply.github.com>
2020-11-04 14:42:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -50,20 +50,20 @@
         echo("<hr>");
         }
 
-    tabelka4("select * from pracownicy", "Wszyscy pracownicy:");
+    tabelka4("select id, imie, id_org, zarobki from pracownicy, organizacja where dzial=id_org", "Wszyscy pracownicy:");
 
-    tabelka4("select * from pracownicy where imie like '%a'", "Kobiety:");
+    tabelka4("select id, imie, id_org, zarobki from pracownicy, organizacja where dzial=id_org and imie like '%a'", "Kobiety:");
     
-    tabelka1("select avg(zarobki) as yavg, dzial from pracownicy group by dzial", "Średnie zarobki pracowników w poszczególnych działach:", "Średnia", "yavg");
+    tabelka1("select avg(zarobki) as yavg, dzial from pracownicy, organizacja group by dzial", "Średnie zarobki pracowników w poszczególnych działach:", "Średnia", "yavg");
 
-    tabelka1("select count(imie) as mycount, dzial from pracownicy where imie not like '%a' group by dzial order by dzial", "Liczba mężczyzn w poszczególnych działach:", "Liczba", "mycount");
+    tabelka1("select count(imie) as mycount, dzial from pracownicy, organizacja where dzial=id_org and imie not like '%a' group by dzial order by dzial", "Liczba mężczyzn w poszczególnych działach:", "Liczba", "mycount");
 
-    tabelka1("select max(zarobki) as mymax, dzial from pracownicy where dzial=2 group by dzial", "Maksymalne zarobki z działu 2:", "Maks", "mymax");
+    tabelka1("select max(zarobki) as mymax, dzial from pracownicy, organizacja where dzial=id_org and dzial=2 group by dzial", "Maksymalne zarobki z działu 2:", "Maks", "mymax");
 
-    tabelka1("select min(zarobki) as mymin, dzial from pracownicy where imie not like '%a' group by dzial", "Minimalne zarobki mężczyzn:", "Min", "mymin");
+    tabelka1("select min(zarobki) as mymin, dzial from pracownicy, organizacja where dzial=id_org nad imie not like '%a' group by dzial", "Minimalne zarobki mężczyzn:", "Min", "mymin");
 
-    tabelka1("select sum(zarobki) as mysum, dzial from pracownicy where dzial=1 or dzial=2 group by dzial", "Suma zarobków z działu 1 i 2:", "Suma", "mysum");
+    tabelka1("select sum(zarobki) as mysum, dzial from pracownicy, organizacja where dzial=id_org and (dzial=1 or dzial=2) group by dzial", "Suma zarobków z działu 1 i 2:", "Suma", "mysum");
  
-    tabelka1("select avg(zarobki) as myavg, dzial from pracownicy group by dzial having myavg<35", "Średnie zarobki pracowników w poszczególnych działach mniejsze od 35:", "Średnia", "myavg");
+    tabelka1("select avg(zarobki) as myavg, dzial from pracownicy, organizacja where dzial=id_org group by dzial having myavg<35", "Średnie zarobki pracowników w poszczególnych działach mniejsze od 35:", "Średnia", "myavg");
 
 ?>
