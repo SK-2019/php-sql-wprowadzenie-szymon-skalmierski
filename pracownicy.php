ff3d05975312562ab729fcfb2d0846beaeb5a1a6
Szymon Skalmierski <73532434+szymon-skalmierski@users.noreply.github.com>
2020-11-06 08:54:56 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,3 +1,18 @@
+<head>
+	<title>Szymon Skalmierski 2Ti</title>
+	<link rel="icon" href="https://image.flaticon.com/icons/png/512/25/25231.png">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1> Szymon Skalmierski nr26 </h1>
+	<a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-szymon-skalmierski"><b>GITHUB</b></a>	
+</body>
+
+	<div class="nav">
+		<a  class="nav1" href="funkcjeagr.php"><b>Funkcje Agregujące</b></a>
+		<a  class="nav2" href="pracownicy.php"><b>Pracownicy</b></a>
+	</div>
+
 <?php
   echo("<div>pracownicy:pracownicy</div>");
 ?>
