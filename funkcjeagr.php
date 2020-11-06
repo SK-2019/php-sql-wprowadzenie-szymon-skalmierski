bd31897b6975aca04c18fc565376f84e8ddfada9
Szymon Skalmierski <73532434+szymon-skalmierski@users.noreply.github.com>
2020-11-06 14:09:58 +0100

    Update funkcjeagr.php

--- funkcjeagr.php
+++ funkcjeagr.php
@@ -49,21 +49,21 @@
      }
 
 
-    tabelka2("select sum(zarobki) as mysum, nazwa_dzial from pracownicy, organizacja where dzial=id_org", "Suma zarobków wszystkich pracowników:", "Suma", "mysum");
+    tabelka2("select sum(zarobki) as mysum from pracownicy", "Suma zarobków wszystkich pracowników:", "Suma", "mysum");
 
-    tabelka2("select sum(zarobki) as mysum, nazwa_dzial from pracownicy, organizacja where dzial=id_org and imie like '%a'", "Suma zarobków wszystkich kobiet:", "Suma", "mysum");
+    tabelka2("select sum(zarobki) as mysum from pracownicy where imie like '%a'", "Suma zarobków wszystkich kobiet:", "Suma", "mysum");
 
     tabelka1("select sum(zarobki) as mysum, nazwa_dzial from pracownicy, organizacja where dzial=id_org and (imie not like '%a') and (dzial=2 or dzial=3) group by dzial", "Suma zarobków mężczyzn pracujących w dziale 2 i 3:", "Suma", "mysum");
 
-    tabelka2("select avg(zarobki) as myavg, nazwa_dzial from pracownicy, organizacja where dzial=id_org and imie not like '%a'", "Średnia zarobków wszystkich mężczyzn:", "Średnia", "myavg");
+    tabelka2("select avg(zarobki) as myavgfrom pracownicy where imie not like '%a'", "Średnia zarobków wszystkich mężczyzn:", "Średnia", "myavg");
 
     tabelka1("select avg(zarobki) as myavg, nazwa_dzial from pracownicy, organizacja where dzial=id_org and dzial=4 group by dzial", "Średnia zarobków pracowników z działu 4:", "Średnia", "myavg");
 
     tabelka1("select avg(zarobki) as myavg, nazwa_dzial from pracownicy, organizacja where dzial=id_org and (imie not like '%a') and (dzial=1 or dzial=2) group by dzial", "Średnia zarobków mężczyzn z działu 1 i 2:", "Średnia", "myavg");
 
-    tabelka2("select count(imie) as mycount, nazwa_dzial from pracownicy, organizacja where dzial=id_org", "Ilu jest wszystkich pracowników:", "Liczba", "mycount");
+    tabelka2("select count(imie) as mycount from pracownicy where dzial=id_org", "Ilu jest wszystkich pracowników:", "Liczba", "mycount");
 
-    tabelka2("select count(imie) as mycount, nazwa_dzial from pracownicy, organizacja where dzial=id_org and imie like '%a' and (dzial=1 or dzial=2)", "Ile kobiet pracuje łącznie w działach 1 i 3:", "Liczba", "mycount");
+    tabelka2("select count(imie) as mycount from pracownicy where dzial=id_org and imie like '%a' and (dzial=1 or dzial=2)", "Ile kobiet pracuje łącznie w działach 1 i 3:", "Liczba", "mycount");
 
 
     tabelka1("select sum(zarobki) as mysum, nazwa_dzial from pracownicy, organizacja where dzial=id_org group by dzial", "Suma zarobków w poszczególnych działach:", "Suma", "mysum");
