ea4405e57e328b6f0964b8d2fb93ccc8358f3673
Szymon Skalmierski <73532434+szymon-skalmierski@users.noreply.github.com>
2020-11-10 01:17:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 </head>
 <body>
 	<h1> Szymon Skalmierski nr26 </h1>
-	<a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-szymon-skalmierski"><b>GITHUB</b></a>	
 </body>
 
 	<div class="nav">
+		<a  class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-szymon-skalmierski"><b>GITHUB</b></a>	
 		<a  class="nav1" href="index.php"><b>Strona Główna</b></a>
 		<a  class="nav1" href="funkcjeagr.php"><b>Funkcje Agregujące</b></a>
 		<a  class="nav1" href="pracownicy.php"><b>Pracownicy</b></a>
