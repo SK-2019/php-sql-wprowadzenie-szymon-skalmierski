1d7476014f6ceed37f4c9fadc9c99ac17bf83d2e
Szymon Skalmierski <73532434+szymon-skalmierski@users.noreply.github.com>
2020-11-14 21:56:36 +0100

    Update data.php

--- data.php
+++ data.php
@@ -51,7 +51,25 @@
         echo("</table>");
         echo("<hr>");
      }
-	tabelka1("select id_pracownicy, imie, nazwa_dzial, zarobki, year(curdate())-year(data_urodzenia) as wiek from pracownicy, organizacja where id_org=dzial", "Pracownicy + wiek:", "Wiek");
+
+        require("connect.php");
+        $result=$conn->query($sql);
+	$sql = 'select id_pracownicy, imie, nazwa_dzial, zarobki, year(curdate())-year(data_urodzenia) as wiek from pracownicy, organizacja where id_org=dzial'
+        echo("<div>Pracownicy + wiek:</div>");
+	    echo("<div class='zapytanie'>$sql</div>");
+        echo("<table class='table1'>");
+            echo("<th>ID</th>");
+	    echo("<th>Imie</th>");
+	    echo("<th>Dział</th>");
+	    echo("<th>Zarobki</th>");
+	    echo("<th>Wiek</th>");
+                while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                        echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['wiek']."</td>");
+                    echo("</tr>");
+		}
+        echo("</table>");
+        echo("<hr>");
 
 	tabelka2("select sum(year(curdate()) - year(data_urodzenia)) as suma from pracownicy", "Suma lat wszystkich pracowników:", "Suma", "suma");
 
