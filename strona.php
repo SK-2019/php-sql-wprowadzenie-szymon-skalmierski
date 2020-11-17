e7c99cf6be2652797d40b583a7e56166c72d0f5e
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-11-18 00:09:53 +0100

    UP

--- strona.php
+++ strona.php
@@ -1,11 +1,20 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+<body class='bodystrona'>
+
 <?php
 
-echo("jesteś na strona.php");
-echo("</br>");
-
-echo("<p>Imie: ".$_POST['imie']);
-echo("<p>Nazwisko: ".$_POST['nazwisko']);
-echo("<p>Klasa: ".$_POST['klasa']);
-echo("<br>");
+echo("<div id='okno1'>");
+	echo("<h2 class='h2strona'>Formularz:");
+	echo("<h3 class='h3strona'>Imie: ".$_POST['imie']);
+	echo("<h3 class='h3strona'>Nazwisko: ".$_POST['nazwisko']);
+	echo("<h3 class='h3strona'>Klasa: ".$_POST['klasa']);
+echo("<div>");
 
 ?>
+
+</body>
+</head>
+</html>
