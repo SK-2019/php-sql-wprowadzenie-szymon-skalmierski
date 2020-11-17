0bd8c2d945dfdb814d6c2f0b74c2d2265bdd29db
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-11-17 23:19:15 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -4,9 +4,9 @@ echo("jesteś na strona.php");
 echo("</br>");
 
 echo($_POST['imie']);
-echo("</br>");
+echo("<br>");
 echo($_POST['nazwisko']);
-echo("</br>");
+echo("<br>");
 echo($_POST['klasa']);
 
 ?>
