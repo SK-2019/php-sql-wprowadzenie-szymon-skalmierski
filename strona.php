33db8a15dc9e893d905f80d898e04ecc9fb27026
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-11-17 23:30:10 +0100

    a

--- strona.php
+++ strona.php
@@ -4,9 +4,7 @@ echo("jesteś na strona.php");
 echo("</br>");
 
 echo("<p>Imie: ".$_POST['imie']);
-echo("<br>");
 echo("<p>Nazwisko: ".$_POST['nazwisko']);
-echo("<br>");
 echo("<p>Klasa: ".$_POST['klasa']);
 echo("<br>");
 
