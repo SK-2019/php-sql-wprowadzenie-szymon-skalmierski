a729bffa909ca6f36b36023ca255c16727c411dd
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-11-18 00:14:55 +0100

    Up

--- strona.php
+++ strona.php
@@ -6,12 +6,12 @@
 
 <?php
 
-echo("<div id='okno1'>");
-	echo("<h2 class='h2strona'>Formularz:");
-	echo("<h3 class='h3strona'>Imie: ".$_POST['imie']);
-	echo("<h3 class='h3strona'>Nazwisko: ".$_POST['nazwisko']);
-	echo("<h3 class='h3strona'>Klasa: ".$_POST['klasa']);
-echo("<div>");
+    echo("<div id='okno1'>");
+        echo("<h2 class='h2strona'>Formularz:");
+        echo("<h3 class='h3strona'>Imię: ".$_POST['imie']);
+        echo("<h3 class='h3strona'>Nazwisko: ".$_POST['nazwisko']);
+        echo("<h3 class='h3strona'>Klasa: ".$_POST['klasa']);
+    echo("<div>");
 
 ?>
 
