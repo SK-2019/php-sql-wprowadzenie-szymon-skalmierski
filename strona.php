a420b9b4511ab3627f72d48b4c46b5a17d1c4845
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-11-18 14:34:44 +0100

    a

--- strona.php
+++ strona.php
@@ -9,7 +9,7 @@
 <?php
 
     echo("<div id='okno1'>");
-        echo("<h2 class='h2strona'>Formularz:");
+        echo("<h2 class='h2strona'>Dodano do bazy danych:");
         echo("<h3 class='h3strona'>Imię: ".$_POST['imie']);
         echo("<h3 class='h3strona'>Nazwisko: ".$_POST['nazwisko']);
         echo("<h3 class='h3strona'>Klasa: ".$_POST['klasa']);
