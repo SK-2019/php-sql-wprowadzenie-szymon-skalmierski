c1fc02903f3adbb3c1b987010d4b0e32bda6286d
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-11-25 14:24:30 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -33,11 +33,11 @@
 
 <?php
     echo("<div id='okno1'>");
-        echo("<h2 class='h2strona'>Dodano do bazy:");
-        echo("<h3 class='h3strona'>Imię: ".$_POST['imie']."</h3>");
-        echo("<h3 class='h3strona'>Dział: ".$_POST['dzial']."</h3>");
-        echo("<h3 class='h3strona'>Zarobki: ".$_POST['zarobki']."</h3>");
-        echo("<h3 class='h3strona'>Data urodzenia: ".$_POST['data_ur']."</h3>");
+    echo("<h2 class='h2strona'>Dodano do bazy:");
+    echo("<h3 class='h3strona'>Imię: ".$_POST['imie']."</h3>");
+    echo("<h3 class='h3strona'>Dział: ".$_POST['dzial']."</h3>");
+    echo("<h3 class='h3strona'>Zarobki: ".$_POST['zarobki']."</h3>");
+    echo("<h3 class='h3strona'>Data urodzenia: ".$_POST['data_ur']."</h3>");
     echo("<div>");
 
 	require_once("connect.php");
