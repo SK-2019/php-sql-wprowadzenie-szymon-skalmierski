4c00fc03b6df193f320f4ca43742e4f33e65a2bf
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-11-25 19:12:13 +0100

    Update

--- pracownicy.php
+++ pracownicy.php
@@ -33,24 +33,29 @@
 
 <?php
     echo("<hr>");
-    function tabelka4($zapytanie, $nazwa){
+    function tabelka4($sql, $nazwa){
         require("connect.php");
-        $result=$conn->query($zapytanie);
+        $result=$conn->query($sql);
         echo("<div>$nazwa</div>");
-	    echo("<div class='zapytanie'>($zapytanie)</div>");
-        echo("<table>");
+        echo("<div class='zapytanie'>($sql)</div>");
+        echo("<table class='tabledel'>");
             echo("<th>ID</th>");
             echo("<th>Imie</th>");
             echo("<th>Dział</th>");
             echo("<th>Zarobki</th>");
+            echo("<th>Data Urodzenia</th>");
+            echo("<th>DELETE</th>");
                 while($row=$result->fetch_assoc()){
                     echo("<tr>");
-                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td><form action='delete.php' method=POST>");
+                    echo("<input type='hidden' name='id' value='".$row['id_pracownicy']."'><input id='delemp1' type='submit' value='USUŃ'>");
+                    echo("</form></td>");
                     echo("</tr>");
                 }
         echo("</table>");
         echo("<hr>");
-        }
-
+    }
     tabelka4("select * from pracownicy, organizacja where dzial=id_org order by id_pracownicy", "Wszyscy pracownicy:");
 ?>
+
