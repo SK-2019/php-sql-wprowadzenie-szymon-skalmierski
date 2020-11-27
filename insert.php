528d8621e93e89c15371a08924dc121f5219c68a
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-11-27 08:22:12 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -48,7 +48,8 @@
       } 
     echo("</div>"); 
 
-    header('Refresh: 5; url=https://git-website-com.herokuapp.com/pracownicy.php');
+    header("location:https://git-website-com.herokuapp.com/daneDoBazy.php");
+    // header('Refresh: 5; url=https://git-website-com.herokuapp.com/pracownicy.php');
 	echo("<div class='redeem1'>Zostaniesz przekierowany na stronę pracowników w ciągu 5 sekund!</div>");  
 ?>
 
