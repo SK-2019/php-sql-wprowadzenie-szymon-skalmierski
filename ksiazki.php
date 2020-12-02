9fb5685a83f96c5197b018b7917d6622ae19bac4
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-12-02 22:18:59 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -54,6 +54,6 @@
         echo("<hr>");
     }
 
-    tabelka("select * from biblTytul,biblAutor,biblAutor_biblTytul where biblAutor_id=bibleTytul_id", "Książki:");
+    tabelka("select * from biblTytul,biblAutor,biblAutor_biblTytul where biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id", "Książki i ich autorzy:");
 
 ?>
