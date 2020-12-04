b921cadf0b9404c1f589cbf47bfa1337da91a0c1
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-12-04 01:12:50 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -69,27 +69,27 @@
 
     tabelka("select bAutor.id as AutorID, bAutor.autor as Autor, bTytul.tytul as Tytul from bAutor, bTytul, bAutor_bTytul where (bAutorID=bAutor.id) and (bTytulID=bTytul.id) order by AutorID", "Książki i ich autorzy:");
 
-    function tabelka1($sql, $nazwa, $thname){
-    require("connect.php");
-    $sql = "select id, autor from bAutor order by id";
-    $result=$conn->query($sql);
-    echo("<div>Autorzy</div>");
-    echo("<div class='zapytanie'>($sql)</div>");
-    echo("<table class='table1'>");
-        echo("<th>ID</th>");
-        echo("<th>$thname</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                echo("<td>".$row['id']."</td><td>".$row['autor']."</td>");
-                echo("</tr>");
-            }
-    echo("</table>");
-    echo("<hr>");
+    function tabelka1($sql, $nazwa, $thname, $row1){
+        require("connect.php");
+        $sql = "select id, autor from bAutor order by id";
+        $result=$conn->query($sql);
+        echo("<div>Autorzy</div>");
+        echo("<div class='zapytanie'>($sql)</div>");
+        echo("<table class='table1'>");
+            echo("<th>ID</th>");
+            echo("<th>$thname</th>");
+                while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                    echo("<td>".$row['id']."</td><td>".$row[$row1]."</td>");
+                    echo("</tr>");
+                }
+        echo("</table>");
+        echo("<hr>");
     }
     
-    tabelka("select id, autor from bAutor order by id", "Autorzy:", "Autorzy");
+    tabelka1("select * from bAutor order by id", "Autorzy:", "Autorzy", 'autor');
 
-    tabelka("select id, tytul from bTytul order by id", "Książki:", "Tytuły");
+    tabelka1("select * from bTytul order by id", "Książki:", "Tytuły", 'tytul');
 
 
 ?>
