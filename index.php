f2fe9b093ded988671a8c33ddd0eb6ab7b207578
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-12-04 08:34:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,13 +22,13 @@
 
 	<div class="nav">
 		<a  class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-szymon-skalmierski"><b>GITHUB</b></a>	
-		<a  class="nav1" href="/index.php"><b>Strona Główna</b></a>
-        <a  class="nav1" href="/pracownicy/pracownicy.php"><b>Pracownicy</b></a>
-		<a  class="nav1" href="/pracownicy/funkcjeagr.php"><b>Funkcje Agregujące</b></a>
-        <a  class="nav1" href="/pracownicy/data.php"><b>Data</b></a>
-        <a  class="nav1" href="/bibl/ksiazki.php"><b>Książki</b></a>
-		<a  class="nav2" href="/pracownicy/formularz.html"><b>Formularz</b></a>
-		<a  class="nav2" href="/pracownicy/daneDoBazy.php"><b>DaneDoBazy</b></a>
+		<a  class="nav1" href="../index.php"><b>Strona Główna</b></a>
+        <a  class="nav1" href="../pracownicy/pracownicy.php"><b>Pracownicy</b></a>
+		<a  class="nav1" href="../pracownicy/funkcjeagr.php"><b>Funkcje Agregujące</b></a>
+        <a  class="nav1" href="../pracownicy/data.php"><b>Data</b></a>
+        <a  class="nav1" href="../bibl/ksiazki.php"><b>Książki</b></a>
+		<a  class="nav2" href="../pracownicy/formularz.html"><b>Formularz</b></a>
+		<a  class="nav2" href="../pracownicy/daneDoBazy.php"><b>DaneDoBazy</b></a>
     </div>
     
 <?php
