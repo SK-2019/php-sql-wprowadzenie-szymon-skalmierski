e270f39a1c53af917b96bb557774082196c193cf
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-12-10 04:14:13 +0100

    Update ksiazki.php

--- bibl/ksiazki.php
+++ bibl/ksiazki.php
@@ -19,7 +19,7 @@
     function tabelka($sql, $nazwa){
         require("../connect.php");
         $result=$conn->query($sql);
-        echo("<table class='table1'>");
+        echo("<table style='width:55%' class='table1'>");
         echo("<caption>");
         echo("<div>$nazwa</div>");
         echo("<div class='zapytanie'>($sql)</div>");
@@ -36,7 +36,22 @@
     }
 
     tabelka("select bAutor.id as AutorID, bAutor.autor as Autor, bTytul.tytul as Tytul from bAutor, bTytul, bAutor_bTytul where (bAutorID=bAutor.id) and (bTytulID=bTytul.id) order by AutorID", "Książki i ich autorzy:");
-
+?>
+    <form class="formularz2" action="inks.php" method="POST">
+        <h2 class="naglowek">Dodawanie nowej książki:</h2>
+	<ul>
+	<li>
+		<input type="text" name="autor" class="field-style field-full" placeholder="Autor"/>
+	</li>
+	<li>
+		<input type="text" name="tytul" class="field-style field-full" placeholder="Tytuł" />
+	</li>
+	<li>
+	<input type="submit" value="Dodaj" />
+	</li>
+	</ul>
+    </form>
+<?php
     function tabelka1($sql, $nazwa, $thname, $row1){
         require("../connect.php");
         $result=$conn->query($sql);
