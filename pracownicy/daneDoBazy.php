ee04cfe3f8fa945ef868b5e1b9f9bfc42c7473e4
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-12-11 04:25:37 +0100

    x

--- pracownicy/daneDoBazy.php
+++ pracownicy/daneDoBazy.php
@@ -4,7 +4,7 @@
 	require("../connect.php");
 	$sql = "select * from pracownicy, organizacja where dzial=id_org order by id_pracownicy";
 		$result=$conn->query($sql);
-		echo("<table class='tabledel' style='width:47%'>");
+		echo("<table class='tabledel'>");
 		echo("<caption style='text-align:left'>");
 		echo("<div class='div1'>Wszyscy pracownicy:</div>");
         echo("<div class='zapytanie'>($sql)</div>");
