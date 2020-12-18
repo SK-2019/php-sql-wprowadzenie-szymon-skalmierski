fe75a83dee7aba37af6b411129d26ecd7d28c998
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2020-12-18 09:05:17 +0100

    x

--- bibl/ksiazki.php
+++ bibl/ksiazki.php
@@ -20,7 +20,7 @@
                         echo("<input type='hidden' name='id' value='".$row['AutorID']."'>");
                         echo("<input type='hidden' name='autor' value='".$row['Autor']."'>");
                         echo("<input type='hidden' name='tytul' value='".$row['Tytul']."'>");
-                        echo("<input id='delemp1' type='submit' class='fas fa-gavel'>");
+                        echo("<button id='delemp2' type='submit' class='fas fa-gavel'>");
                     echo("</form></td>");
                     echo("</tr>");
                 }
