83dc9f5280cb90fddf9f778df9b04ce8fbe7a0ea
Szymon Skalmierski <szymon.skalmierski123@gmail.com>
2021-02-03 16:11:43 +0100

    Update

--- biblioteka/wypozyczone.php
+++ biblioteka/wypozyczone.php
@@ -29,45 +29,48 @@
         echo("</table>");
         
                 
-        echo '<form class="formularz2" action="wypozyczone.php" method="POST">';
-        echo '<h2 class="naglowek">Szukanie książki przez autora:</h2>';
+        echo("<div class='formularz0'>"); 
+        echo '<form action="wypozyczone.php" method="POST">';
+        echo '<h2 class="naglowek">Wypożyczenie książki:</h2>';
+        echo("<div class='formbox'>"); 
         echo '<ul>';
         echo '<li>';
                 $sql=('SELECT autor, id FROM bAutor');
                 $result=$conn->query($sql);               
-        echo '<select name="autor" class="field-style field-full">';
+        echo '<select name="autor" class="field-style field-split align-right">';
             while($row=$result->fetch_assoc()){
                 echo("<option value='".$row['autor']."'>".$row['autor']."</option>");
             }
         echo '</select>';
         echo '</li>';
         echo '<li>';
-        echo '<input type="submit" value="Sprawdź" />';
+        echo '<input type="submit" value="Sprwadź" />';
         echo '</li>';
         echo '</ul>';
         echo '</form>';
 
         $autor = $_POST['autor'];
-        echo '<form class="formularz2" action="inwy.php" method="POST">';
-        echo '<h2 class="naglowek">Wypożyczenie książki:</h2>';
+        echo '<form action="inwy.php" method="POST">';
         echo '<ul>';
         echo '<li>';
         $sql = "select bTytulID, bTytul.id as tytulID, bAutor.id as AutorID, bAutor.autor as Autor, bTytul.tytul as Tytul from bAutor, bTytul, bAutor_bTytul where (bAutorID=bAutor.id) and (bTytulID=bTytul.id) and (bAutor.autor = '$autor')";
                 $result=$conn->query($sql);
-        echo '<select name="tytul" class="field-style field-full">';
+        echo '<select name="tytul" class="field-style field-split align-right">';
             while($row=$result->fetch_assoc()){
                 echo("<option value='".$row['Tytul']."'>".$row['Tytul']."</option>");
             }
         echo("</select>");
-        echo '<select hidden name="autorid" class="field-style field-full">';
+        echo '<select hidden name="autorid" class="field-style field-split align-left">';
                 echo("<option value='$autor'></option>");
         echo("</select>");
         echo '</li>';
         echo '<li>';
-        echo '<input type="submit" value="Wypożycz" />';
+        echo '<input type="submit" value="Wyślij" />';
         echo '</li>';
         echo '</ul>';
         echo '</form>';
+        echo ("</div>");
+        echo ("</div>");
 ?>
     </div>
 </div>
