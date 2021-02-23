c9a6394dd41522aa51093d9bb78e30459a1b7ed8
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-02-23 13:49:42 +0100

    Update

--- assets/connect.php
+++ assets/connect.php
@@ -1,10 +1,16 @@
 <?php
+
 $hostname = $_SERVER['HTTP_HOST'];
 if ($hostname == 'localhost:8003') {
     require_once ("config.php");
 }
 
-$conn=new mysqli($_SERVER['servername'],$_SERVER['username'],$_SERVER['password'],$_SERVER['dbname']);
+$servername = $_SERVER['servername'];
+$username = $_SERVER['username'];
+$password = $_SERVER['password'];
+$dbname = $_SERVER['dbname'];
+
+$conn=new mysqli($servername, $username, $password, $dbname);
         if($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
