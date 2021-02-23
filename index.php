b05d67465e7d6fd17723d1d284ddbfc9cb2e1d08
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-02-23 12:54:13 +0100

    Update

--- index.php
+++ index.php
@@ -21,27 +21,31 @@
         echo("</table>");
         }
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and (dzial=1 or dzial=3) and imie like '%a' order by zarobki", "Kobiety z działu 1 i 3 posortowane po zarobkach:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and (dzial=1 or dzial=3) and imie like '%a' order by zarobki", "Kobiety z działu 1 i 3 posortowane po zarobkach:");
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and dzial=3 order by imie", "Pracownicy z działu 3 posortowani po imieniu:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and dzial=3 order by imie", "Pracownicy z działu 3 posortowani po imieniu:");
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and dzial=2", "Pracownicy z działu 2:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and dzial=2", "Pracownicy z działu 2:");
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and (dzial=2 or dzial=3)", "Pracownicy z działu 2 i 3:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and (dzial=2 or dzial=3)", "Pracownicy z działu 2 i 3:");
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and zarobki<30", "Pracownicy z zarobkami mniejszymi niż 30:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and zarobki<30", "Pracownicy z zarobkami mniejszymi niż 30:");
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and (dzial=1 or dzial=4)", "Pracownicy z działu 1 i 4:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and (dzial=1 or dzial=4)", "Pracownicy z działu 1 i 4:");
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and imie like '%a'", "Kobiety:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and imie like '%a'", "Kobiety:");
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and imie not like '%a'", "Mężczyźni:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and imie not like '%a'", "Mężczyźni:");
 
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org order by imie desc", "Pracownicy posortowani malejąco wg imienia:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org order by imie desc", "Pracownicy posortowani malejąco wg imienia:");
  
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and imie like '%a' order by imie", "Kobiety posortowane rosnąco po imieniu:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and imie like '%a' order by imie", "Kobiety posortowane rosnąco po imieniu:");
     
-    tabelka4("select * from pracownicy, organizacja where dzial=id_org and imie not like '%a' order by nazwa_dzial asc, zarobki asc", "Mężczyźni posortowani rosnąco: po nazwie działu a następnie po wysokości zarobków:");
+    // tabelka4("select * from pracownicy, organizacja where dzial=id_org and imie not like '%a' order by nazwa_dzial asc, zarobki asc", "Mężczyźni posortowani rosnąco: po nazwie działu a następnie po wysokości zarobków:");
+
+    $hostname = $_SERVER['HTTP_HOST'];
+    echo("<li> hostname: ".$hostname);
+
 ?>
     
 </div>
