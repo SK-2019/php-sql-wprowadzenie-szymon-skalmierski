19a1594916425c93163497df4aa7a8dfe040f420
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-04-20 02:01:02 +0200

    Update

--- github/wiele.php
+++ github/wiele.php
@@ -1,13 +1,14 @@
 <?php include "../assets/body.html" ?>
     <div class='phpp'>
 <?php
+        // PRACOWNICY I DZIAŁY
     echo("<div class='wiele1'>");
     require("../assets/connect.php");
     $result=$conn->query('select * from pracownik,dzial where (dzial_id=dzial.id) order by nazwaDzial');
-        echo("<table style='width:55%'>");
+        echo("<table style='width:50%'>");
         echo("<caption>");
         echo("<div class='div1'>Pracownicy w działach:</div>");
-        echo("<div class='zapytanie'>('select * from pracownik,dzial where (dzial_id=dzial.id) order by nazwaDzial')</div>");
+        echo("<div class='zapytanie'>(select * from pracownik,dzial where (dzial_id=dzial.id) order by nazwaDzial)</div>");
         echo("</caption>");
             echo("<th>Nazwa Działu</th>");
             echo("<th>Imię</th>");
@@ -21,7 +22,7 @@
         echo("</table>");
     
     $result=$conn->query("select * from pracownik order by id");
-        echo("<table style='width:55%'>");
+        echo("<table style='width:50%;'>");
         echo("<caption>");
         echo("<div class='div1'>Pracownicy:</div>");
         echo("<div class='zapytanie'>(select * from pracownik order by id)</div>");
@@ -53,4 +54,56 @@
                 }
         echo("</table>");
     echo("</div>");
+
+
+    // KSIĄŻKI I AUTORZY
+    echo("<div class='wiele1'>");
+    require("../assets/connect.php");
+    $result=$conn->query('select *, autor.id as autorid from autor,tytul,autor_tytul where (autor.id=autor_id) and (tytul.id=tytul_id) order by autor.id');
+        echo("<table style='width:50%'>");
+        echo("<caption>");
+        echo("<div class='div1'>Autorzy i ich dzieła:</div>");
+        echo("<div class='zapytanie'>(select *, autor.id as autorid from autor,tytul,autor_tytul where (autor.id=autor_id) and (tytul.id=tytul_id) order by autor.id)</div>");
+        echo("</caption>");
+            echo("<th>ID</th>");
+            echo("<th>Nazwisko</th>");
+            echo("<th>Tytuł</th>");
+                while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                    echo("<td>".$row['autorid']."</td><td>".$row['nazwisko']."</td><td>".$row['tytul']."</td>");
+                    echo("</tr>");
+                }
+        echo("</table>");
+
+    $result=$conn->query("select * from tytul order by id");
+        echo("<table style='width:50%'>");
+        echo("<caption>");
+        echo("<div class='div1'>Tytuły:</div>");
+        echo("<div class='zapytanie'>(select * from tytul order by id)</div>");
+        echo("</caption>");
+            echo("<th>ID</th>");
+            echo("<th>Tytuł</th>");
+                while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                    echo("<td>".$row['id']."</td><td>".$row['tytul']."</td>");
+                    echo("</tr>");
+                }
+        echo("</table>");
+    
+    $result=$conn->query("select * from autor order by id");
+        echo("<table style='width:30%;'>");
+        echo("<caption>");
+        echo("<div class='div1'>Autorzy:</div>");
+        echo("<div class='zapytanie'>(select * from autor order by id)</div>");
+        echo("</caption>");
+            echo("<th>ID</th>");
+            echo("<th>Nazwisko</th>");
+                while($row=$result->fetch_assoc()){
+                    echo("<tr>");
+                    echo("<td>".$row['id']."</td><td>".$row['nazwisko']."</td>");
+                    echo("</tr>");
+                }
+        echo("</table>");
+    echo("</div>");
+    
 ?>
