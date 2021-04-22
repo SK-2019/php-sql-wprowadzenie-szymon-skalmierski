794462372bc1c3e76d35a069c33d6c84db379d1e
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-04-22 02:48:29 +0200

    Update

--- pracownicy/tabelaf.php
+++ pracownicy/tabelaf.php
@@ -4,8 +4,8 @@
 
     tabela("SELECT * FROM pracownik", "Wszyscy pracownicy");
 
-    tabela("SELECT * FROM pracownik where (dzial_id=1 or dzial_id=3)","Pracownicy z działu 1 i 3");
+    tabela("SELECT * FROM pracownik where (dzial_id=1 or dzial_id=3)","Pracownicy z działu 1 i 3:");
 
-    tabela("SELECT * FROM pracownik where (dzial_id=2 or dzial_id=4)","Pracownicy z działu 2 i 4");
+    tabela("SELECT * FROM pracownik where (dzial_id=2 or dzial_id=4)","Pracownicy z działu 2 i 4:");
 
 ?>
