1e3820db59c1fc2bcde1a12c432f2eb6d2d59273
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-04-25 02:42:06 +0200

    Update

--- wiele/wiele6/insertwiele6.php
+++ wiele/wiele6/insertwiele6.php
@@ -4,7 +4,7 @@
 	$sql = "INSERT INTO pracownik(`id`, `imie`, `dzial_id`, `wynagrodzenie`, `dataUrodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial_id']."', '".$_POST['wynagrodzenie']."', '".$_POST['dataUrodzenia']."')";
 	$conn->query($sql);
 
-    header("location:wiele6.php");
+    // header("location:wiele6.php");
 ?>
     </div>
 </div>
