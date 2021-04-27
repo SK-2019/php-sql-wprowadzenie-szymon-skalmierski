5d7d17686fb7178600ee4cffbaa18ed372befc0c
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-04-27 14:29:42 +0200

    Update

--- wiele/wiele1/insertwiele1.php
+++ wiele/wiele1/insertwiele1.php
@@ -1,21 +1,17 @@
 <head>
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 window.onload = function(){
   document.getElementById('clickButton').click();
 }
-</script>
+</script> -->
 </head>
 <?php include "/app/assets/body.html" ?>
 <?php
     require("/app/assets/connect.php");
         $sql = "INSERT INTO osoba(id, imie, nazwisko) VALUES(NULL, '".$_POST['imie']."', '".$_POST['nazwisko']."')";
-        echo("<h3 class='h3strona'>Autor: ".$_POST['autor']."</h3>");
-        echo("<br>");
         $conn->query($sql);
 
         $sql = "INSERT INTO rola(id, nazwaRoli) VALUES(NULL, '".$_POST['rola']."')";
-        echo("<h3 class='h3strona'>Tytuł: ".$_POST['tytul']."</h3>");
-        echo("<br>");
         $conn->query($sql);
 
 
@@ -24,7 +20,8 @@ window.onload = function(){
         $result=$conn->query($sql);
         echo("<table>");
             echo("<th>ID</th>");
-            echo("<th>Autor</th>");
+            echo("<th>Imie</th>");
+            echo("<th>Nazwisko</th>");
                 while($row=$result->fetch_assoc()){
                     echo("<tr>");
                         echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["nazwisko"]."</td>");
