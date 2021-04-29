6a6330eebedd598ff6c96d7e25ad011c97e6dc13
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-04-29 14:48:15 +0200

    Update

--- wiele/wiele1/delwiele1.php
+++ wiele/wiele1/delwiele1.php
@@ -5,7 +5,10 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require_once("/app/assets/connect.php");
-echo(".$_POST['mid'].");
+print_r($_POST['mid']);
+print_r($_POST['oid']);
+print_r($_POST['rid']);
+
     if(!empty($_POST['mid']))
     {
         $sql = "DELETE FROM osoba_rola WHERE id='".$_POST['mid']."'";
