cef3a394fa2ff185e77fb784f0604e2c335a5be0
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-04-29 14:52:23 +0200

    Update

--- wiele/wiele1/delwiele1.php
+++ wiele/wiele1/delwiele1.php
@@ -5,27 +5,26 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require_once("/app/assets/connect.php");
-print_r($_POST['mid']);
 
     if(!empty($_POST['mid']))
     {
         $sql = "DELETE FROM osoba_rola WHERE id='".$_POST['mid']."'";
         $conn->query($sql); 
-        // header("location:wiele1.php"); 
+        header("location:wiele1.php"); 
     }
 
     if(!empty($_POST['oid']))
     {
         $sql = "DELETE FROM osoba WHERE id='".$_POST['oid']."'";
         $conn->query($sql);
-        // header("location:wiele1.php");  
+        header("location:wiele1.php");  
     }
 
     if(!empty($_POST['rid']))
     {
         $sql = "DELETE FROM rola WHERE id='".$_POST['rid']."'";
         $conn->query($sql); 
-        // header("location:wiele1.php");
+        header("location:wiele1.php");
     }
  
 ?>
