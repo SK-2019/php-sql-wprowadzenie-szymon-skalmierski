0896027c7719b03b9079cbf1c4a293977ced5b72
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-04-29 17:24:23 +0200

    Update

--- wiele/fundel.php
+++ wiele/fundel.php
@@ -31,9 +31,9 @@ function tabdel($sql, $nazwa, $file, $name, $rowname, $tclass)
             {           
                 echo "<td>";
                 echo $totalRows[$eachRecord];
-                echo "</td>"; 
-                echo("<td><form action=$file method=POST><input type='text' name=$name value='".$row[$rowname]."'><input id='delemp1' type='submit' value='X'></form></td>");              
+                echo "</td>";              
             }
+            echo "<td><form action=$file method=POST><input type='text' name=$name value='".$row[$rowname]."'><input id='delemp1' type='submit' value='X'></form></td>"; 
             echo "</tr>";           
         } 
         echo "</table>";        
