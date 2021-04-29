d7b931f2bdb5b442817c551273a96839acb5ac22
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-04-29 12:47:35 +0200

    Update

--- wiele/wiele1/delwiele1.php
+++ wiele/wiele1/delwiele1.php
@@ -1,5 +1,8 @@
 <?php include "/app/assets/body.html" ?>
 <?php
+echo ".$_POST['mid'].";
+echo ".$_POST['rid'].";
+echo ".$_POST['oid'].";
     if($_POST('mid')!="")
     {
         require_once("/app/assets/connect.php");
@@ -15,7 +18,7 @@
         $conn->query($sql);
         header("location:wiele1.php");  
     }
-    
+
     if($_POST('rid')!="")
     {
         require_once("/app/assets/connect.php");
