58cfddf4ab03809e200c9bfaf7e0db17016c2a2b
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-05-11 13:47:43 +0200

    Update

---
+++ github/galeria.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="galeriastyles.css" />
+  </head>
+  <body>
+    <h2 class="heading">Galeria:</h2>
+
+    <div class="gallery row">
+      <div class="image photo01">1</div>
+      <div class="image photo02">2</div>
+      <div class="image photo03">3</div>
+    </div>
+
+    <div class="gallery row">
+      <a href="" class="image photo01">
+        <span class="photo01_title">kurs jazdy moplikiem</span>
+        <span class="photo01_price">cena: 299 zł</span>
+      </a>
+      <a href="" class="image photo02">
+        <span class="photo01_title">kurs wspinaczkowy</span>
+        <span class="photo01_price">cena: 299 zł</span>
+      </a>
+      <a href="" class="image photo03">
+        <span class="photo01_title">kurs nurkowania</span>
+        <span class="photo01_price">cena: 1299 zł</span>
+      </a>
+    </div>
+    <div class="gallery row">
+      <div class="image photo01">1</div>
+      <div class="image photo02">2</div>
+      <div class="image photo03">3</div>
+    </div>
+  </body>
+</html>
