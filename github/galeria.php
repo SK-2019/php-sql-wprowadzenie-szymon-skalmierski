c34c4524df9d98c417fd05b4770adafc09405f34
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-05-11 14:29:07 +0200

    Update

--- github/galeria.php
+++ github/galeria.php
@@ -8,10 +8,18 @@
     <h2 class="galleryheading">Galeria:</h2>
 
     <div class="gallery row">
-      <div class="image photo01"></div>
-      <div class="image photo02"></div>
-      <div class="image photo03"></div>
-    </div>
+      <div class="image photo01">
+        <span class="photo01_title">kurs surfingu</span>
+        <span class="photo01_price">cena: 699 zł</span>
+      </div>
+      <div class="image photo02">
+        <span class="photo01_title">kurs wspinaczkowy</span>
+        <span class="photo01_price">cena: 499 zł</span>
+      </div>
+      <div class="image photo03">
+        <span class="photo01_title">kurs fotografii</span>
+        <span class="photo01_price">cena: 899 zł</span>
+      </div>
 
     <div class="gallery row">
       <a href="" class="image photo01">
@@ -28,9 +36,18 @@
       </a>
     </div>
     <div class="gallery row">
-      <div class="image photo01"></div>
-      <div class="image photo02"></div>
-      <div class="image photo03"></div>
+      <div class="image photo01">
+        <span class="photo01_title">kurs surfingu</span>
+        <span class="photo01_price">cena: 699 zł</span>
+      </div>
+      <div class="image photo02">
+        <span class="photo01_title">kurs wspinaczkowy</span>
+        <span class="photo01_price">cena: 499 zł</span>
+      </div>
+      <div class="image photo03">
+        <span class="photo01_title">kurs fotografii</span>
+        <span class="photo01_price">cena: 899 zł</span>
+      </div>
     </div>
   </body>
 </html>
