89ced29d36f278684f3a89b603f20f5ce951e1fc
szymon-skalmierski <szymon.skalmierski123@gmail.com>
2021-05-11 14:31:00 +0200

    Update

--- github/galeria.php
+++ github/galeria.php
@@ -9,43 +9,43 @@
 
     <div class="gallery row">
       <div class="image photo01">
-        <span class="photo01_title">kurs surfingu</span>
-        <span class="photo01_price">cena: 699 zł</span>
+        <span class="photo01_title">kurs surfingu STANDARD</span>
+        <span class="photo01_price">cena: 399 zł</span>
       </div>
       <div class="image photo02">
-        <span class="photo01_title">kurs wspinaczkowy</span>
-        <span class="photo01_price">cena: 499 zł</span>
+        <span class="photo01_title">kurs wspinaczkowy STANDARD</span>
+        <span class="photo01_price">cena: 299 zł</span>
       </div>
       <div class="image photo03">
-        <span class="photo01_title">kurs fotografii</span>
-        <span class="photo01_price">cena: 899 zł</span>
+        <span class="photo01_title">kurs fotografii STANDARD</span>
+        <span class="photo01_price">cena: 399 zł</span>
       </div>
 
     <div class="gallery row">
       <a href="" class="image photo01">
-        <span class="photo01_title">kurs surfingu</span>
-        <span class="photo01_price">cena: 699 zł</span>
-      </a>
-      <a href="" class="image photo02">
-        <span class="photo01_title">kurs wspinaczkowy</span>
+        <span class="photo01_title">kurs surfingu VIP</span>
         <span class="photo01_price">cena: 499 zł</span>
       </a>
+      <a href="" class="image photo02">
+        <span class="photo01_title">kurs wspinaczkowy VIP</span>
+        <span class="photo01_price">cena: 399 zł</span>
+      </a>
       <a href="" class="image photo03">
-        <span class="photo01_title">kurs fotografii</span>
-        <span class="photo01_price">cena: 899 zł</span>
+        <span class="photo01_title">kurs fotografii VIP</span>
+        <span class="photo01_price">cena: 499 zł</span>
       </a>
     </div>
     <div class="gallery row">
       <div class="image photo01">
-        <span class="photo01_title">kurs surfingu</span>
+        <span class="photo01_title">kurs surfingu PRO</span>
         <span class="photo01_price">cena: 699 zł</span>
       </div>
       <div class="image photo02">
-        <span class="photo01_title">kurs wspinaczkowy</span>
+        <span class="photo01_title">kurs wspinaczkowy PRO</span>
         <span class="photo01_price">cena: 499 zł</span>
       </div>
       <div class="image photo03">
-        <span class="photo01_title">kurs fotografii</span>
+        <span class="photo01_title">kurs fotografii PRO</span>
         <span class="photo01_price">cena: 899 zł</span>
       </div>
     </div>
